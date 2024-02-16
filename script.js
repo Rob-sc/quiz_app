e71fb549eda741e2b02f809c147ce8f91bbca67b
Roberto Sanchez Camacho <robert@macrob.local>
2024-02-16 13:49:25 +0100

    start javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+const questions = 
+[
+    {
+        "question": "¿Cuál es el animal más grande?",
+        "answers": {
+            "A": "Perro",
+            "B": "Rata",
+            "C": "Elefante",
+            "D": "Pingüino"
+        }
+    },
+    {
+        "question": "¿Qué hacemos hoy para comer?",
+        "answers": {
+            "A": "Perro",
+            "B": "Rata",
+            "C": "Elefante",
+            "D": "Pingüino"
+        }
+    },
+    {
+        "question": "¿Quien la tine más grande?",
+        "answers": {
+            "A": "Perro",
+            "B": "Rata",
+            "C": "Elefante",
+            "D": "Pingüino"
+        }
+    }
+]
+
+const question = document.getElementById('question') 
